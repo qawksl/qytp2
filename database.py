84296bf2a5facb72a76c3074bcb9ca2a7f03f604
H8YOU212 <slavyan.212@mail.ru>
2024-11-08 11:58:40 +0500

    add form i td

--- database.py
+++ database.py
@@ -19,8 +19,26 @@ class Database():
         result_raw = self.connaction.execute(query).all()
         return self.translate_to_dict(result_raw)
     
+    def del_animals(self,id):
+        query = sqla.text("DELETE FROM animals WHERE id = :id")
+        query = query.bindparams(sqla.bindparam("id", id))
+        self.connaction.execute(query)
+        self.connaction.commit()        
+
+    def add_animals(self, name):
+        query = sqla.text("INSERT INTO animals (name) VALUES (:name) ")
+        query = query.bindparams(sqla.bindparam("name", name))
+        self.connaction.execute(query)
+        self.connaction.commit()
+
+    def edit_animals(self, name, id):
+        query = sqla.text("UPDATE animals SET name = :name WHERE id = :id")
+        query = query.bindparams(sqla.bindparam("name", name))
+        query = query.bindparams(sqla.bindparam("id", id))
+        self.connaction.execute(query)
+        self.connaction.commit()
   
     
 if __name__ == "__main__":
-    db = database.Database()
+    db = Database()
     print(db.get_animals())

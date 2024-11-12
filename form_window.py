b99437570e010d51161dbb93df1558b63d70f995
H8YOU212 <slavyan.212@mail.ru>
2024-11-12 12:24:01 +0500

    все работает добавление

--- form_window.py
+++ form_window.py
@@ -3,44 +3,32 @@ from PyQt6.QtWidgets import QMainWindow,QDialog,QLabel,QHBoxLayout, QPushButton,
 from database import Database
 
 class FormWindow(QDialog):
-    def __init__(self, parent = None,object = None):
+    def __init__(self, parent= None, object = None):
         super().__init__(parent)
         if object is None:
-            self.setWindowTitle("Форма добавления")
+            self.setWindowTitle("Добавление")
             self.button = QPushButton("Добавить")
-            self.button.clicked.connect(self.button_click)
+            self.button.clicked.connect(self.close)
         else:
-            self.setWindowTitle("Форма изменения")
+            self.setWindowTitle("Изменение")
             self.button = QPushButton("Изменить")
+            self.button.clicked.connect(self.close)
             self.id = object.split(":")[0]
-            self.button.clicked.connect(self.button_click)
-
-
-
-        label = QLabel("Название:")
-        self.name_text = QLineEdit(object)
-        self.id = object.split(":")[0]
-
+        label = QLabel("Название")
         if object is None:
-            self.name_text.text = QLineEdit()
+            self.name_text = QLineEdit()
         else:
             self.name_text = QLineEdit(object.split(":")[1])
-
-        if object is None:
-            self.name_text = object
-
+    
         name_input = QHBoxLayout()
         name_input.addWidget(label)
         name_input.addWidget(self.name_text)
         name_input_widget = QWidget()
         name_input_widget.setLayout(name_input)
-
-
         layout = QVBoxLayout()
         layout.addWidget(name_input_widget)
         layout.addWidget(self.button)
 
         self.setLayout(layout)
-
-    def button_click(self):
-         self.done(1)
+    def close(self):
+        self.done(1) 
